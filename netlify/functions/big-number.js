ec96316a038c590ee3737b7a08d20a1268ab00c4
Emanuel Tesar <e.tesarr@gmail.com>
2021-09-07 15:49:18 +0200

    Increase the number returned

--- netlify/functions/big-number.js
+++ netlify/functions/big-number.js
@@ -1,6 +1,6 @@
 export async function handler(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ value: '12345678987654321123456789' }),
+    body: JSON.stringify({ value: '123456789123456789123456789123456789123456789123456789' }),
   }
 }
